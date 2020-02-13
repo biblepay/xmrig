ba861cb55a0c0705f96410d431f6cb45cec8e1ab
biblepay <contact@biblepay.org>
2020-02-13 10:06:08 -0600

    5.5.5n-Mandatory Upgrade for TestNet
    - Refresh mine_example.bat to point to bitness of filename

--- binaries/mine_example.bat
+++ binaries/mine_example.bat
@@ -1,1 +1,1 @@
-xmrig.exe -o rxtest.biblepay.org:3008 -u your_bbp_receive_address -p your_monero_receive_address --threads 4
+xmrig-bbp-win64.exe -o rxtest.biblepay.org:3008 -u your_bbp_receive_address -p your_monero_receive_address --threads 4
